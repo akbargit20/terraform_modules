0a8bb13f889287c26eefa21a44d9643c25504baf
sans8 <48122608+sans8@users.noreply.github.com>
2021-06-09 14:48:48 +0530

    Add files via upload

---
+++ public_subnet/outputs.tf
@@ -0,0 +1,5 @@
+output "pub_subnet_id" {
+  value       = aws_subnet.public-subnet.id
+  description = "Public Subnet ID"
+}
+
---
+++ public_subnet/vars.tf
@@ -0,0 +1,12 @@
+variable "pub_cidr_block" {
+  description = "Public CIDR Block"
+}
+
+variable "vpc_id" {
+  description = "VPC ID"
+}
+
+variable "availability_zone" {
+  description = "Availability Zone"
+}
+
